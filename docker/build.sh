caf988cb10df3c18cf16712638800ac0f39baaab
k9lego <k9lego@users.noreply.github.com>
2020-11-30 16:21:11 -0500

    Build and Upload Compiled Executables
    Builds for Windows 32-bit, Linux Debian, and MacOS
    Note: These binaries are held for the "Actions>Artifact and log retention number of days" setting (default 90) then deleted.

--- docker/build.sh
+++ docker/build.sh
@@ -1,44 +1,86 @@
-#!/usr/bin/env bash
-
-# Abort at the first error.
-set -e
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJECT_DIR="$( cd "${SCRIPT_DIR}/.." && pwd )"
-
-# For debugging.
-echo "GITHUB_REF: ${GITHUB_REF}"
-echo "GITHUB_HEAD_REF: ${GITHUB_HEAD_REF}"
-echo "GITHUB_BASE_REF: ${GITHUB_BASE_REF}"
-
-GIT_REF_NAME_LIST=( "${GITHUB_HEAD_REF}" "${GITHUB_BASE_REF}" "${GITHUB_REF}" "master" )
-for git_ref_name in "${GIT_REF_NAME_LIST[@]}"
-do
-  if [ -z "${git_ref_name}" ]; then
-    continue
-  fi
-  git_ref_name="$(basename "${git_ref_name}")"
-  # Skip refs/pull/1234/merge as pull requests use it as GITHUB_REF
-  if [[ "${git_ref_name}" == "merge" ]]; then
-    echo "Skip [${git_ref_name}]"
-    continue
-  fi
-  SERIOUS_PROTON_BRANCH="${git_ref_name}"
-  output="$(git ls-remote --heads https://github.com/Daid/SeriousProton "${SERIOUS_PROTON_BRANCH}")"
-  if [ -n "${output}" ]; then
-    echo "Found SeriousProton branch [${SERIOUS_PROTON_BRANCH}]."
-    break
-  else
-    echo "Could not find SeriousProton banch [${SERIOUS_PROTON_BRANCH}], try next."
-  fi
-done
-
-echo "Using SeriousProton branch ${SERIOUS_PROTON_BRANCH} ..."
-
-git clone --depth=1 -b "${SERIOUS_PROTON_BRANCH}" https://github.com/Daid/SeriousProton.git "${PROJECT_DIR}"/SeriousProton
-
-mkdir build
-cd build
-cmake .. -DSERIOUS_PROTON_DIR=$PROJECT_DIR/SeriousProton/
-make
-
+---
+name: CI/CD
+on:
+  push:
+    branches:
+      - master
+  pull_request:
+jobs:
+  ubuntu:
+    name: Build Linux on Ubuntu
+    runs-on: ubuntu-latest
+    steps:
+    - name: Update Container
+      run: sudo apt-get update -qq
+    - name: Instal SFML
+      run: sudo apt-get install libsfml-dev
+    - name: Checkout EmptyEpsilon
+      uses: actions/checkout@v2
+    - name: Build Linux Deb
+      run: docker/build.sh linux
+    - name: Upload EmptyEpsilon.deb artifact
+      uses: actions/upload-artifact@v2
+      with:
+        name: Linux Debian
+        path: /home/runner/work/EmptyEpsilon/EmptyEpsilon/build/EmptyEpsilon.deb
+        if-no-files-found: error
+    - name: Upload script_reference.html artifact
+      uses: actions/upload-artifact@v2
+      with:
+        name: Script Reference
+        path: /home/runner/work/EmptyEpsilon/EmptyEpsilon/build/script_reference.html
+        if-no-files-found: error
+  win32:
+    name: Build Win32 on Ubuntu
+    runs-on: ubuntu-latest
+    steps:
+    - name: Update Container
+      run: sudo apt-get update -qq
+    - name: Install tools and SFML
+      run: sudo apt-get install build-essential cmake python3-minimal mingw-w64 ninja-build p7zip-full libsfml-dev
+    - name: Checkout EmptyEpsilon
+      uses: actions/checkout@v2
+    - name: Build Windows 32-bit
+      run: docker/build.sh win32
+    - name: Upload Win32 Artifact
+      uses: actions/upload-artifact@v2
+      with:
+        name: Windows
+        path: /home/runner/work/EmptyEpsilon/EmptyEpsilon/_build_win32/upload/
+        if-no-files-found: error
+  macos:
+    name: Build Mac on MacOS
+    runs-on: macos-latest
+    steps:
+    - name: Update Container
+      run: sudo softwareupdate -ia
+    - name: Install SFML
+      run: brew install cmake sfml
+    - name: Checkout EmptyEpsilon
+      uses: actions/checkout@v2
+    - name: Build MacOS
+      run: docker/build.sh macos
+    - name: Upload MacOS Artifact
+      uses: actions/upload-artifact@v2
+      with:
+        name: MacOS
+        path: /Users/runner/work/EmptyEpsilon/EmptyEpsilon/_build/EmptyEpsilon.dmg
+        if-no-files-found: error
+  android:
+    name: Build Android on Ubuntu
+    runs-on: ubuntu-latest
+    steps:
+    - name: Update Container
+      run: sudo apt-get update -qq
+    - name: Install Tools and SFML
+      run: sudo apt-get install build-essential cmake python3-minimal mingw-w64 ninja-build p7zip-full libsfml-dev
+    - name: Checkout EmptyEpsilon
+      uses: actions/checkout@v2
+    - name: Build Android
+      run: docker/build.sh android
+    - name: Upload Android Artifact
+      uses: actions/upload-artifact@v2
+      with:
+        name: Android
+        path: /home/runner/work/EmptyEpsilon/EmptyEpsilon/_build_android/EmptyEpsilon.apk
+        if-no-files-found: error
